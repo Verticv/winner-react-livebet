fd1d6bcba69e5c469214156628e5bfcfa27acfc6
Mohammed_Alghazali <malghazali@itgsoftware.com>
2022-04-19 22:57:05 +0300

    Fix issue  #11

--- src/components/Cart/BetSlipBets/BetSlipBets.jsx
+++ src/components/Cart/BetSlipBets/BetSlipBets.jsx
@@ -1,15 +1,22 @@
-import React from 'react'
-import useStore from 'store/useStore';
-import BetSlipBet from './BetSlipBet';
+import React from "react";
+import useStore from "store/useStore";
+import BetSlipBet from "./BetSlipBet";
 import "./BetSlipBets.scss";
 
 export default function BetSlipBets() {
-    const betSlipBets = useStore(s => s.betSlipBets)
+    const betSlipBets = useStore((s) => s.betSlipBets);
+
     return (
-        <div className='cart-cards-wrapper'>
-            {betSlipBets.map((bet, index) => {
-                return <BetSlipBet key={bet.id + index} bet={bet}/>
-            })}
+        <div className="cart-cards-wrapper">
+            {betSlipBets.length ? (
+                betSlipBets.map((bet, index) => {
+                    return <BetSlipBet key={bet.id + index} bet={bet} />;
+                })
+            ) : (
+                <div className="cart-card-wrapper-multi">
+                    베팅할 게임을 선택하세요.
+                </div>
+            )}
         </div>
     );
 }
