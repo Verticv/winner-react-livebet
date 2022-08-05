275acc51d7b5df5cf9bc0a44b47e8d1836039338
Andrew Jang <dhj9817@gmail.com>
2022-08-05 14:28:08 +0900

    .

--- src/components/MultiViewMatchCard/MultiViewMatchCard.jsx
+++ src/components/MultiViewMatchCard/MultiViewMatchCard.jsx
@@ -514,7 +514,7 @@ export default function MultiViewMatchCards() {
 
     return (
         <div className="multi-view-match-results-wrapper" 
-        style={{height : `${(window.innerHeight - 310)}px`}}
+        style={{height : `${(window.innerHeight - 260)}px`}}
         >
             {console.log('window.innerHeight', window.innerHeight)}
             {resultsCardsList.map(({ id, isEmpty }, index) => {
